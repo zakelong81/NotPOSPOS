7fe6fe0914b4f1aa5ae1747057b6090a65f326b2
Kevin Buffardi <kbuffardi@csuchico.edu>
2017-11-08 11:41:06 -0800

    Begins main program and removes extraneous files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "order.h"
+
+//the user's actions
+int main()
+{
+  std::cout<<"======================\n"
+           <<"Welcome to Not POS POS\n"
+           <<"======================\n";
+  order current;
+  return 0;
+}
